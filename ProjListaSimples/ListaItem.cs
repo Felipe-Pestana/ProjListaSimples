f7c905092c77ce157e744b3f9552fc2b44a2f890
Felipe Pestana <pestana@sitic.com.br>
2023-03-16 16:43:34 -0300

    Add project files.

---
+++ ProjListaSimples/ListaItem.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjListaSimples
+{
+    internal class ListItem
+    {
+        public Item Begin { get; set; }
+        public Item End { get; set; }
+
+        public ListItem()
+        {
+            Begin = null;
+            End = null;
+        }
+
+        public void Insert(Item i)
+        {
+            if (IsEmpty())
+            {
+                Begin = i;
+                End = i;
+            }
+            else
+            {
+                //Find(i);
+                if(i.Value <= Begin.Value)
+                {
+                    InsertFirst(i);
+                }
+                else
+                {
+                    if (i.Value >= End.Value)
+                    {
+                        InsertLast(i);
+                    }
+                    else 
+                    {
+                        InsertMiddle(i);
+                    }
+                }
+
+            }
+        }
+
+        public void InsertLast(Item i)
+        {
+            End.Next = i;
+            End = i;
+
+        }
+
+        public void InsertFirst(Item i)
+        {
+            i.Next = Begin;
+            Begin = i;
+        }
+
+        public bool Find(Item f) 
+        {
+            Item aux = Begin;
+            while (aux.Next != null)
+            {
+                if(aux.Value == f.Value)
+                {
+                    return true;
+                }
+                else
+                {
+                    aux = aux.Next;
+                }
+                return false;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return true;
+        }
+
+        public void Print() 
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Lista Vazia.");
+            }
+            else
+            {
+                Find();
+            }
+        }
+    }
+}
---
+++ ProjListaSimples/Program.cs
@@ -0,0 +1,24 @@
+﻿using ProjListaSimples;
+
+ListItem list = new ListItem();
+
+Item item = new(21);
+
+list.Insert(item);
+list.Print();
+
+item = new(25);
+list.Insert(item);
+list.Print();
+
+item = new(40);
+list.Insert(item);
+list.Print();
+
+item = new(10);
+list.Insert(item);
+list.Print();
+
+item = new(22);
+list.Insert(item);
+list.Print();
